12c1bf2c3b205ff645db7a4b03b57659d896e109
0xNaN <enzodicicco@gmail.com>
2015-10-28 15:36:19 +0100

    adding second test

--- src/Game.java
+++ src/Game.java
@@ -1,11 +1,13 @@
 
 public class Game {
-
-	public void roll(int i) {
+	private int score = 0;
+	
+	public void roll(int pins) {
+		this.score += pins;
 	}
 
 	public int score() {
-		return 0;
+		return score;
 	}
 
 }
--- test/BowlingGameTest.java
+++ test/BowlingGameTest.java
@@ -13,4 +13,13 @@ public class BowlingGameTest {
 		}
 		assertEquals(0, game.score());
 	}
+	
+	@Test
+	public void testAllOnes() {
+		Game game = new Game();
+		for(int i = 0; i < 20; i++) {
+			game.roll(1);
+		}
+		assertEquals(20, game.score());
+	}
 }
