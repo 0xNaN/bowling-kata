1e50d4d6b31ecba52ca1dcae77d1ce1dd88435b1
0xNaN <enzodicicco@gmail.com>
2015-10-29 10:27:47 +0100

    refactoring

--- src/Game.java
+++ src/Game.java
@@ -1,13 +1,19 @@
 
 public class Game {
-
-	private int score;
-
+	private static final int MAX_NUMBER_OF_ROLLS = 20;
+	private int rolls[] = new int[MAX_NUMBER_OF_ROLLS + 1];
+	private int currentRoll = 0;
+	
 	public void roll(int pins) {
-		score += pins;
+		rolls[currentRoll] = pins;
+		currentRoll++;
 	}
 
 	public int score() {
+		int score = 0;
+		for(int roll = 0; roll < rolls.length; roll++) {
+			score += rolls[roll];
+		}
 		return score;
 	}
 
--- test/GameTest.java
+++ test/GameTest.java
@@ -1,25 +1,45 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class GameTest {
+	private static final int MAX_ROLLS = 20;
+	private Game game;
+	
+	@Before
+	public void setUp() {
+		game = new Game();
+	}
 
 	@Test public void
 	score_is_zero_if_no_pins_was_hit() {
-		Game game = new Game();
-		for(int i = 0; i < 20; i++) {
-			game.roll(0);
-		}
+		rollMany(MAX_ROLLS, 0);
 		assertEquals(0, game.score());
 	}
 	
 	@Test public void
 	score_is_twenty_if_one_pin_is_hit_for_roll() {
-		Game game = new Game();
-		for(int i = 0; i < 20; i++) {
-			game.roll(1);
-		}
+		rollMany(MAX_ROLLS, 1);
 		assertEquals(20, game.score());
+	}
+	
+//	@Test public void
+//	adding_bonus_if_spare() {
+//		// spare
+//		game.roll(2);
+//		game.roll(8);
+//		// bonus
+//		game.roll(5);
+//		rollMany(MAX_ROLLS - 3, 0);
+//		
+//		assertEquals(20, game.score());
+//	}
+
+	private void rollMany(int rolls, int pins) {
+		for(int i = 0; i < rolls; i++) {
+			game.roll(pins);
+		}
 	}
 }
